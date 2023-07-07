353907cbdfc268686b394021f7febeafaeb7fcca
andres-frick <66190289+andres-frick@users.noreply.github.com>
2023-07-06 22:02:55 -0300

    Update main.js 3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.getElementById("header").innerHTML=`<nav class="navbar navbar-expand-sm">
 <div class="container">
-  <a class="navbar-brand" href="/">Agenda</a>
+  <a class="navbar-brand" href="/agenda-tpo/">Agenda</a>
   <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
       aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
